b986275eccf66c00a852c06ac8565fea24a79897
hyunn2 <48798620+hyunn2@users.noreply.github.com>
2020-08-29 04:15:58 +0900

    Update and rename 9358.c to 1182.c

---
+++ 1182.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	main(void)
+{
+	int N;
+	int S;
+	int count = 0;
+	int *array;
+	int i = 0;
+	int input;
+	scanf("%d %d", &N, &S);
+	if (!(N >= 1 && N <= 20))
+		return;
+	if (!(array = (int*)malloc(sizeof(int) * N)))
+		return (0);
+	while (i < N)
+		scanf("%d", &array[i++]);
+	i = 0;
+	while (array[i] > 0)
+	{
+		i++;
+	}
+}
