0a84665867f1ea16550f4876d035fbfd159f1ffb
hyunn2 <48798620+hyunn2@users.noreply.github.com>
2020-08-29 04:18:58 +0900

    Update 1182.c

--- 1182.c
+++ 1182.c
@@ -9,12 +9,16 @@ int	main(void)
 	int i = 0;
 	int input;
 	scanf("%d %d", &N, &S);
+	getchar();
 	if (!(N >= 1 && N <= 20))
 		return;
 	if (!(array = (int*)malloc(sizeof(int) * N)))
 		return (0);
 	while (i < N)
+	{
 		scanf("%d", &array[i++]);
+		getchar();
+	}
 	i = 0;
 	while (array[i] > 0)
 	{
