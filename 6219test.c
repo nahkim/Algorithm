72f12811cbc81e143bf0eba0fe4725676c8cf854
hyunn2 <48798620+hyunn2@users.noreply.github.com>
2020-10-14 06:40:44 +0900

    Create 6219test.c

---
+++ 6219test.c
@@ -0,0 +1,37 @@
+int	ft_cmp(char src, char find)
+{
+	if (src == find)
+		return (1);
+	return (0);
+}
+
+
+int find(char *min, char *str, char *max)
+{
+	int res = 0;
+	int minlen = ft_strlen(min) - 1;
+	int maxlen = ft_strlen(max) - 1;
+	int strlen = ft_strlen(str) - 1;
+	i = 0;
+	j = 0;
+	
+	while(minlen - strlen - 1 > i)
+	{
+		if(ft_cmp(min[i], str[j])
+		{
+			i++;
+			j++;
+			if (str[j] == NULL)
+			{
+				res++;
+				j = 0;
+			}
+		}
+		else
+		{
+			i++;
+			j = 0;
+		}
+	}
+	return (res);
+}
