e529ba3e1cc83496c7129c5097f0c5acf5607bdb
hyunn2 <48798620+hyunn2@users.noreply.github.com>
2020-10-17 05:02:08 +0900

    Update 6219test.c

--- 6219test.c
+++ 6219test.c
@@ -1,47 +1,121 @@
-int	ft_cmp(char src, char find)
+//time out
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int digit_count(int n)
 {
-	if (src == find)
-		return (1);
-	return (0);
+    int i = 1;
+
+    while (n >= 10)
+    {
+        i++;
+        n /= 10;
+    }
+    return (i);
 }
 
-
-int find(char *min, char *str)
+char *ft_itoa(int n)
 {
-	int res = 0;
-	int minlen = ft_strlen(min) - 1;
-	int strlen = ft_strlen(str) - 1;
-	i = 0;
-	j = 0;
-	
-	while(minlen - (strlen - 1) > i)
+    int len;
+    char *str;
+
+    len = digit_count(n);
+    str = (char *)malloc(sizeof(char) * len + 1);
+    str[len] = '\0';
+    while (len-- > 0)
+    {
+        str[len] = n % 10 + '0';
+        n = n / 10;
+    }
+    return (str);
+}
+
+int ft_count(int *str)
+{
+    int i = 0;
+    while (str[i] != 0)
+        i++;
+    return (i);
+}
+
+int isprime(int n)
+{
+	int i = 2;
+
+	while (i < n)
 	{
-		if(ft_cmp(min[i], str[j])
-		{
+		if (n % i != 0)
 			i++;
-			j++;
-			if (str[j] == NULL)
-			{
-				res++;
-				j = 0;
-			}
-		}
 		else
-			j = 0;
+			return (0);
 	}
-	return (res);
+	if (n == 1)
+		return (0);
+	return (1);
+}
+
+int ft_find(char *min, char *find)
+{
+    int res = 0;
+    int i = 0;
+    int j = 0;
+
+    while (min[i])
+    {
+        if (min[i] == find[j])
+        {
+            j++;
+            if (!find[j])
+            {
+                j = 0;
+                res++;
+            }
+        }
+        else
+        {
+            j = 0;
+        }
+        i++;
+    }
+    return (res);
 }
 
 int main(void)
 {
-	int min;
-	int max;
-	int find;
-	
-	while (min <= max)
-	{
-		res += find(min, str);
-		min++;
-	}
-	return (res);
+    int min;
+    int max;
+    int find;
+    int save[400000] = {0,};
+    int i = 0;
+    int res = 0;
+    int count = 0;
+    char *src;
+    char *dest;
+
+    scanf("%d %d %d", &min, &max, &find);
+    while (max - min >= 0)
+    {
+        if (isprime(min))
+        {
+            save[i] = min;
+            //printf("prime : %d\n", save[i]);
+            i++;    
+        }
+        min++;
+    }
+    count = ft_count(save);
+    i = 0;
+    dest = ft_itoa(find);
+    while (i < count)
+    {
+        src = ft_itoa(save[i]);
+        res += ft_find(src, dest);
+        i++;
+        free(src);
+    }
+    printf("%d", res);
+    free(dest);
+    return (0);
 }
