2502f1010bc471b950b8a0e03f2f3b98b0fefb7f
hyunn2 <48798620+hyunn2@users.noreply.github.com>
2020-11-23 18:39:32 +0900

    Create 11650.c
    연결리스트 구조체 생성

---
+++ 11650.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct	s_node
+{
+	struct node *next;
+	int x;
+	int y;
+}t_node;
+
+int	main(void)
+{
+	int index = 0;
+	int i = 0;
+	
+	scanf("%d", &index);
+	getchar();
+	while (i < index)
+	{
+		scanf("%d %d", );
+		getchar();
+	}
+	
+	return (0);
+}
