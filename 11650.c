f9e43a69e9f4af91ed8f86df9b1b7bbd9c55ca2e
hyunn2 <nahkim@student.42seoul.kr>
2020-11-25 16:55:37 +0900

    Update 11650.c

--- 11650.c
+++ 11650.c
@@ -2,16 +2,60 @@
 #include <stdlib.h>
 
 
-//typedef struct s_node t_node;
 typedef struct	s_node
 {
-	struct node *next;
+	struct s_node *next;
 	int x;
 	int y;
 }	t_node;
 
+void	Ascending_array(t_node *head)
+{
+	t_node *cur;
+	t_node *tmp;
+	int	check = 1;
+
+	//cur = head->next;
+	while (check > 0)
+	{
+		check = 0;
+		cur = head->next;
+		while (cur->next != NULL)
+		{
+			if (cur->x > cur->next->x)
+			{
+				//
+				tmp = cur->next;
+				cur = cur->next->next;
+				tmp->next = head->next;
+				head->next = tmp;
+				check += 1;
+			}
+			else if (cur->x == cur->next->x)
+			{
+				if (cur->y > cur->next->y)
+				{
+					//
+					check += 1;
+				}
+				else
+				{
+					cur = cur->next;
+					check += 0;
+				}
+			}
+			else
+			{
+				cur = cur->next;
+				check += 0;
+			}
+		}
+	}
+}
+
 void	print_node(t_node *cur)
 {
+	cur = cur->next;
 	while (cur != NULL)
 	{
 		printf("%d %d\n", cur->x, cur->y);
@@ -45,26 +89,33 @@ void	free_node(t_node *target)
 int	main(void)
 {
 	t_node	*head;
-	t_node	*cur= head->next;
+	//t_node	*cur;
 	int index;
-	int i = 0;
 	int x = 0;
 	int y = 0;
 	
 	if ((head = (t_node *)malloc(sizeof(t_node))) == NULL)
 		return (0);
 	head->next = NULL;
+	//if ((cur = (t_node *)malloc(sizeof(t_node))) == NULL)
+	//	return (0);
 	//cur = head->next;
 	scanf("%d", &index);
 	getchar();
-	while (i < index)
+	while (index-- > 0)
 	{
 		scanf("%d %d", &x, &y);
 		getchar();
 		add_node(head, x, y);
-		i++;
 	}
+	Ascending_array(head);
 	print_node(head);
 	free_node(head);
+	/*
+	while(1)
+	{
+		;
+	}
+	*/
 	return (0);
 }
