06a1b01b6e481b17f1f465ed7c50837e2d39b759
hyunn2 <48798620+hyunn2@users.noreply.github.com>
2021-01-02 06:13:18 +0900

    20210102

--- 8979.c
+++ 8979.c
@@ -2,26 +2,35 @@
 
 int main(void)
 {
-    int num, target, i, target1;
-    int n, gold, silver, bronze;
-    int res = 1;
+    int num, target, i;
 
     scanf("%d %d", &num, &target);
     getchar();
-    long long array[num][2];
-    for (i = 0; i < num; i++)
+    int array[num][4];
+    int res = 1;
+    int n, gold, silver, bronze;
+
+
+    for (i = 0; i < num; i++)   //각 나라 및 메달 수 저장
     {
-        scanf("%d %d %d %d", &n, &gold, &silver, &bronze);
-        array[i][0] = n;
-        array[i][1] = gold * 1e12 + silver * 1e6 + bronze;
-        if (n == target)
-            target1 = i;
+        scanf("%d %d %d %d", &array[i][0], &array[i][1], &array[i][2], &array[i][3]);
+        getchar();
+        if (array[i][0] == target)  // 찾고자하는 나라의 메달 수 저장
+        {
+            gold = array[i][1];
+            silver = array[i][2];
+            bronze = array[i][3];
+        }
     }
-    for (i = 0; i < num; i++)
+    for (i = 0; i < num; i++)   // 비교
     {
-        if (array[i][1] > array[target1][1])
+        if (array[i][1] > gold)
+            res++;
+        else if (array[i][1] == gold && array[i][2] > silver)
+            res++;
+        else if (array[i][1] == gold && array[i][2] == silver && array[i][3] > bronze)
             res++;
     }
-    printf("%d", res);    // 결과 출력
+    printf("%d", res);  // 결과 출력
     return (0);
 }
