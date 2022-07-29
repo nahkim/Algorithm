a56833939e8b4f193344d9f4ebebbc1241d13103
nahkim <nahkim@student.42seoul.kr>
2022-07-29 16:21:36 +0900

    0729 풀이 완료

--- 7568.py
+++ 7568.py
@@ -1,39 +1,19 @@
-import sys
+# import sys
 
-sys.stdin = open("input.txt", "r")
+# sys.stdin = open("input.txt", "r")
 
-test_case = int(input())
-
-x_list = []
-y_list = []
-res = [0 for i in range(test_case)]
-# print(res)
-rank = 1
-
-for i in range(test_case):
+T = int(input())
+people_info = []
+ranks = []
+for i in range(T):
     x, y = map(int, input().split())
-    x_list.append(x)
-    y_list.append(y)
-
-while sum(x_list) != -test_case:
-    max_x = max(x_list)
-    max_y = max(y_list)
-    found_x = x_list.index(max_x)
-    found_y = y_list.index(max_y)
-    if found_x == found_y:
-        res[found_x] = rank
-        rank += 1
-        x_list[found_x] = -1
-        y_list[found_y] = -1
-    else:
-        for i in range(test_case):
-            if y_list[i] > y_list[found_x] or x_list[i] > x_list[found_y]:
-                res[i] = rank
-                x_list[i] = -1
-                y_list[i] = -1
-            elif y_list[i] == y_list[found_x] or x_list[i] == x_list[found_y]:
-                res[i] = rank
-                x_list[i] = -1
-                y_list[i] = -1
-        rank += 1
-print(res)
+    people_info.append((x, y))
+    # print(people_info)
+    # print(len(people_info))
+rank = 1
+for i in people_info:
+    for j in people_info:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end=' ')
+    rank = 1
