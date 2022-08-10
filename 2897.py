3575558ad1ccb53208896bdd04b7b0c45274ff24
nahkim <nahkim@student.42seoul.kr>
2022-08-10 09:39:13 +0900

    0810 fix

--- 2897.py
+++ 2897.py
@@ -16,20 +16,17 @@ for k in range(5):
     for x in range(r):              # 행 만큼 반복문
         for y in range(c):          # 열 만큼 반복문
             temp_count = 0          # 2 * 2 공간에 들어가 있는 차 수
-            check = 0
             for d in range(4):
                 nx = x + dx[d]
                 ny = y + dy[d]
                 if not(0 <= nx < r and 0 <= ny < c):    # 예외 조건(인덱스 범위를 벗어난 경우)
-                    check = -1
                     break
                 if parking[nx][ny] == '#':              # 예외 조건(빌딩인 경우)
                     temp_count = 0
-                    check = -1
                     break
                 elif parking[nx][ny] == 'X':            # 주차할 공간에 차가 있다면
                     temp_count += 1                     # 부순 차 더하기
-            if check != -1:                             # 예외 조건에 들어갔을 경우 확인하기 위해 사용
+            else:                                       # 예외 조건에 들어가지 않았을 경우
                 if temp_count == k:                     # 부수고 들어갈 차 갯수 k와 부순 차 temp_count가 같다면
                     total += 1                          # 총 갯수 + 1
     print(total)
