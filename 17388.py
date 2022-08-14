ea9b8203b11938bf1c8b4d1fcf6ee912efc04c12
nahkim <nahkim@student.42seoul.kr>
2022-08-15 02:21:54 +0900

    220813 fin

--- 17388.py
+++ 17388.py
@@ -4,7 +4,7 @@ if (sum(list_)) >= 100:
     print('OK')
 else:
     min_univ = min(list_)
-    num = list_.index(min_uni)
+    num = list_.index(min_univ)
     if num == 0:
         print('Soongsil')
     elif num == 1:
