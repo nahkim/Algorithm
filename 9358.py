5e17c0e8f29a6d6d6b60a5ec05b6a0bbf72bc63f
nahkim <nahkim@student.42seoul.kr>
2023-01-17 02:30:16 +0900

    :white_check_mark: 9358 재귀
    재귀

--- 9358.py
+++ 9358.py
@@ -1,21 +1,18 @@
 import sys
 
-# sys.stdin = open("input.txt", "r")
+def fold(arr):
 
-def fold(n, arr):
+    if len(arr) == 2:
+        return arr
+
     new_arr = []
-
-    if n % 2 == 1:
-        cnt = n // 2 + 1
-    else:
-        cnt = n // 2
     i = 0
-    j = n - 1
+    j = len(arr) - 1
     while i <= j:
         new_arr.append(arr[i] + arr[j])
         i += 1
         j -= 1
-    return new_arr
+    return fold(new_arr)
 
 
 T = int(sys.stdin.readline())
@@ -25,11 +22,11 @@ for i in range(1, T + 1):
     arr = list(map(int, sys.stdin.readline().split()))
     new_arr = arr
     while True:
-        new_arr = fold(len(new_arr), new_arr)
+        new_arr = fold(new_arr)
         if len(new_arr) == 2:
             break
     if new_arr[0] > new_arr[1]:
-        print("Case #", T, ": Alice", sep="")
+        print("Case #", i, ": Alice", sep="")
     else:
-        print("Case #", T, ": Bob", sep="")
+        print("Case #", i, ": Bob", sep="")
     
