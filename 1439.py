770b4fc5394275411999b543e84e5670256a8153
nahkim <nahkim@student.42seoul.kr>
2023-01-27 00:35:02 +0900

    :white_check_mark: 1439 그리디
    그리디 알고리즘

--- 1439.py
+++ 1439.py
@@ -12,34 +12,22 @@ check_one = "1"
 count_zero_start = 0
 count_one_start = 0
 
-# 1이면 count 추가
 before_num = arr[0]
 if arr[0] == '1':
     count_zero_start += 1
+else:
+    count_one_start += 1
 for i in range(1, len(arr)):
-    if check_zero == arr[i]:
-        before_num = arr[i]
-        continue
-    else:
+    # 1이면 count 추가
+    if check_zero != arr[i]:
         count_zero_start += 1
         if before_num == arr[i]:
             count_zero_start -= 1
-            continue
-        before_num = arr[i]
-  
-# 0이면 count 추가
-before_num = arr[0]
-if arr[0] == '0':
-    count_one_start += 1
-for i in range(1, len(arr)):
-    if check_one == arr[i]:
-        before_num = arr[i]
-        continue
-    else:
+    # 0이면 count 추가
+    elif check_one != arr[i]:
         count_one_start += 1
         if before_num == arr[i]:
             count_one_start -= 1
-            continue
-        before_num = arr[i]
+    before_num = arr[i]
 
 print(min(count_zero_start, count_one_start))
