58c3dd29a88e1ff2f580f991c4a5c7985832113d
nahkim <nahkim@student.42seoul.kr>
2023-03-13 00:53:24 +0900

    :bug: 3107.py 구현
    1:2:3:4:5:6:7:: 예제시 오류 수정

--- 3107.py
+++ 3107.py
@@ -2,17 +2,19 @@ import sys
 
 s_list = sys.stdin.readline().strip().split(":")
 res = ""
+check = True
 
 for s in s_list:
     if s == "":
-        res += "0000:"
+        if check:
+            for i in range(8 - len(s_list) + 1):
+                res += "0000:"
+            check = False
+        else:
+            res += "0000:"
     else:
         if len(s) != 4:
             for i in range(4 - len(s)):
                 res += "0"
-            res += s
-        else:
-            res += s
-        res += ":"
-        
-print(res)
+        res += s + ":"
+print(res[:-1])
