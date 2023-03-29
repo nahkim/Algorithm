e6ec898af97b37be8b06a151c068d97e0e707619
nahkim <nahkim@student.42seoul.kr>
2023-03-29 23:31:10 +0900

    :white_check_mark: 1526 브루트포스
    가장 큰 금민수

--- 1526.py
+++ 1526.py
@@ -1,5 +1,16 @@
 n = int(input())
 
 res = 4
-while res < n:
-    
+while res <= n:
+    check = 0
+    s_n = str(n)
+    for i in range(len(s_n)):
+        if s_n[i] == '4' or s_n[i] == '7':
+            continue
+        else:
+            n -= 1
+            check = 1
+            break
+    if check == 0:
+        print(n)
+        break
