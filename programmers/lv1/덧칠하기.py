79cf1436937fb047884fc262c44645f8be483138
nahkim <nahkim@student.42seoul.kr>
2023-04-14 22:17:49 +0900

    :recycle: 덧칠하기 구현
    중복 코드 제거

--- programmers/lv1/덧칠하기.py
+++ programmers/lv1/덧칠하기.py
@@ -13,3 +13,17 @@ def solution(n, m, section):
             i = j + m - 1
             answer += 1
     return answer
+
+# 코드 줄임(속도는 더 느린듯)
+def solution(n, m, section):
+    answer = 0
+    i = section[0]
+    
+    if m == 1:
+        return len(section)
+    
+    for j in section:
+        if i <= j:
+            i = j + m
+            answer += 1
+    return answer
