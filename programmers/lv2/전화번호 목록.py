c7f49cb4eb922d05a0b6851eed5505fb859be02b
nahkim <nahkim@student.42seoul.kr>
2023-04-29 23:56:53 +0900

    :bug: lv2 전화번호 목록 / list
    정확성 75, 효율성 16.7 | 91.7/100

--- programmers/lv2/전화번호 목록.py
+++ programmers/lv2/전화번호 목록.py
@@ -1,8 +1,8 @@
 def solution(phone_book):
     answer = True
-    for i in range(len(phone_book)):
-        for j in range(len(phone_book)):
-            if i != j and phone_book[j] in phone_book[i]:
-                answer = False
-                return answer
+    phone_book.sort()
+    for i in range(len(phone_book) - 1):
+        if phone_book[i + 1] in phone_book[i] or phone_book[i] in phone_book[i + 1]:
+            answer = False
+            return answer
     return answer
