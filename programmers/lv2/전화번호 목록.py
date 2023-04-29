262561f3bb430a88278bec34cee9b991b5c7cfef
nahkim <nahkim@student.42seoul.kr>
2023-04-30 00:01:56 +0900

    :white_check_mark: lv2 전화번호 목록 / list
    list

--- programmers/lv2/전화번호 목록.py
+++ programmers/lv2/전화번호 목록.py
@@ -2,7 +2,7 @@ def solution(phone_book):
     answer = True
     phone_book.sort()
     for i in range(len(phone_book) - 1):
-        if phone_book[i + 1] in phone_book[i] or phone_book[i] in phone_book[i + 1]:
+        if phone_book[i] in phone_book[i + 1][:len(phone_book[i])]:
             answer = False
             return answer
     return answer
