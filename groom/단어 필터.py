f3328b7935cca0e4a117d94c40e0a7dc21ef543a
nahkim <nahkim@student.42seoul.kr>
2023-05-04 17:06:39 +0900

    :poop: 구름 1주차 단어 필터
    17/20

---
+++ groom/단어 필터.py
@@ -0,0 +1,21 @@
+s_len, e_len = map(int, input().split())
+s = input()
+e = input()
+
+res = ''
+i = 0
+while i <= e_len - s_len:
+	if e[i:i + s_len] == s:
+		i += s_len
+	else:
+		res += e[i]
+		i += 1
+
+while i < e_len:
+	res += e[i]
+	i += 1
+
+if res == '':
+	print('EMPTY')
+else:
+	print(res)
