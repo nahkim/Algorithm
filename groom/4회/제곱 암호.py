3f804772a989a89e7c1fb1a498c7d852488134c4
nahkim <nahkim@student.42seoul.kr>
2023-05-13 15:25:01 +0900

    :white_check_mark: 제곱 암호 | 완전 탐색
    아스키 코드

--- groom/4회/제곱 암호.py
+++ groom/4회/제곱 암호.py
@@ -6,14 +6,12 @@ user_input = input()
 
 res = ''
 i = 0
+# for i in range(0, n, 2):
 while i < n:
 	c = user_input[i]
 	cnt = int(user_input[i + 1])
 	cnt = cnt ** 2
-	change_c = ord(c) + cnt
-	if change_c > ord('z'):
-		change_c -= ord('z')
-		change_c += ord('a') - 1
+	change_c = (ord(c) + cnt - ord('a')) % 26 + ord('a')
 	res += chr(change_c)
 	i += 2
 print(res)
