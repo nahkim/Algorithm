c8c5c223a2eb588ceca6a18f21469003377d650d
nahkim <nahkim@student.42seoul.kr>
2023-05-19 23:27:12 +0900

    :white_check_mark: 직사각형 만들기 | 딕셔너리, 우선순위 큐
    딕셔너리, 우선순위 큐

--- groom/6회/직사각형 만들기.py
+++ groom/6회/직사각형 만들기.py
@@ -1,14 +1,13 @@
 from heapq import heappop, heappush
 import sys
-# input = sys.stdin.readline
 
 n = int(input())
 nums = list(map(int, input().split()))
 
 nums_dict = {}
 heap = []
-res = 1
-cnt = 0
+pair = []
+res = 0
 
 for num in nums:
 	if num not in nums_dict:
@@ -17,17 +16,16 @@ for num in nums:
 		nums_dict[num] += 1
 	heappush(heap, -num)
 
+
 while heap:
 	tmp = -heappop(heap)
 	if nums_dict[tmp] >= 2:
-		print(tmp)
 		heappop(heap)
-		res *= tmp
-		cnt += 1
-	if cnt == 2:
-		break
+		pair.append(tmp)
+		nums_dict[tmp] -= 2
 
-if res == 1:
-	print(0)
-else:
-	print(res)
+for i in range(0, len(pair) // 2 * 2, 2):
+	res += pair[i] * pair[i + 1]
+	
+print(res)
+	
